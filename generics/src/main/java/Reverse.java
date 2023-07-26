264a2318bfd53d1eed55925001ebce7336bceb57
Erkan <ErkanOzulu@users.noreply.github.com>
2023-07-26 19:14:22 +0300

    Reverse

---
+++ generics/src/main/java/Reverse.java
@@ -0,0 +1,34 @@
+public class Reverse {
+    public static void main(String[] args) {
+
+
+        String str="Bryan Adams";
+
+
+        String reversedStr="";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversedStr+=str.charAt(i);
+
+        }
+
+        System.out.println("reversedStr = " + reversedStr);
+
+        System.out.println("===========second way==============");
+
+         //StringBuffer is thread-safe and synchronize but slow
+
+        StringBuffer str1=new StringBuffer(str);
+        System.out.println("str1.reverse() = " + str1.reverse());
+
+        System.out.println("===========third way==============");
+
+        //StringBuffer is not thread-safe and synchronize but fast
+
+        StringBuilder str2=new StringBuilder(str);
+
+        System.out.println("str2.reverse() = " + str2.reverse());
+
+
+    }
+}
