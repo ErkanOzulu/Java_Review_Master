0e2bc6fd0d07094cc27a8529e7cd6a1a61e48c21
Erkan <ErkanOzulu@users.noreply.github.com>
2023-08-02 06:05:12 +0300

    collections review

--- collections-review/src/Student.java
+++ collections-review/src/Student.java
@@ -1,7 +1,9 @@
+import java.util.Objects;
+
 public class Student {
 
-int id;
-String name;
+    int id;
+    String name;
 
     public Student(int id, String name) {
         this.id = id;
@@ -15,4 +17,18 @@ String name;
                 ", name='" + name + '\'' +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
+
