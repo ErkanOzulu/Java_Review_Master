2f5703a1971636ab5cb155ee0030c1fcd714f8e5
Erkan <ErkanOzulu@users.noreply.github.com>
2023-08-10 13:04:37 +0300

    behavioral parameterization

--- lambda_/src/main/java/applePredicate/ApplePredicate.java
+++ lambda_/src/main/java/applePredicate/ApplePredicate.java
@@ -4,3 +4,7 @@ public interface ApplePredicate {
 
     boolean test(Apple apple);
 }
+
+
+//it should be only have one abstract method to implement lambda.(Functional interface)
+// if you have more than one abstract method you cannot implement  lambda express. and you need to do behavioral parameterization
--- lambda_/src/main/java/arraySorting/Sorting.java
+++ lambda_/src/main/java/arraySorting/Sorting.java
@@ -1,5 +1,14 @@
 package arraySorting;
+//it should be only have one abstract method to implement lambda.(Functional interface)
 @FunctionalInterface
 public interface Sorting {
     public void sort();
 }
+
+
+/**
+    to apply behavioral parametrization, the rule has three steps
+ 1- prepare an interface --> allows creating a method with doesn't have any implementation
+ 2- Create a class that implement to that interface and give implementation
+ 3- Create an object from the implementation class and give as a behavior
+ */
