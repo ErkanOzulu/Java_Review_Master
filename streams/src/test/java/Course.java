f587e21b06687eea85e3080754a2245219ee12d9
Erkan <ErkanOzulu@users.noreply.github.com>
2023-08-14 21:21:37 +0300

    StreamOperations

---
+++ streams/src/test/java/Course.java
@@ -0,0 +1,9 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+@AllArgsConstructor
+@Data
+public class Course {
+    private String courseName;
+    private Integer courseId;
+}
