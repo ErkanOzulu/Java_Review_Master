9dd563ceea08fad5d43ff95aab177233c773f05b
Erkan <ErkanOzulu@users.noreply.github.com>
2023-08-15 21:54:16 +0300

    state Task(flatMap)

---
+++ streams/src/test/java/Tasks/States/StateTest.java
@@ -0,0 +1,43 @@
+package Tasks.States;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StateTest {
+
+    public static void main(String[] args) {
+
+        // Suppose we have a State class that contains list of cities. Create a list of States and print all the cities.
+
+
+        State va=new State();
+        va.addCity("McLean");
+        va.addCity("Arlington");
+        va.addCity("Fairfax");
+        va.addCity("FallsChurch");
+
+        State tx=new State();
+        tx.addCity("Dallas");
+        tx.addCity("Plano");
+        tx.addCity("Austin");
+        tx.addCity("Houston");
+
+
+        List<State>stateList= Arrays.asList(va,tx);
+
+        stateList.stream().map(state -> state.getCity())
+                        .forEach(System.out::println);
+
+        System.out.println("****************");
+
+        stateList.stream().map(state -> state.getCity())
+                .flatMap(List::stream)
+                .forEach(System.out::println);
+
+
+
+
+
+
+    }
+}
